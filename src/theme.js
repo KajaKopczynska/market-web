3ceaccc5f824ab7641b18d5ba5c846f1746b6388
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-07-05 12:22:25 +0200

    theme.js added

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        white: "#FFFFFF",
+
+        black: "#000000",
+    },
+
+    breakpoints: {
+        medium: 555,
+        large: 992,
+        max: 1250,
+        mobileMin: 471,
+        mobileMax: 767,
+    },
+};
