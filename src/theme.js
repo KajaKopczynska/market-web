f59bf09abc86cb5683b16a01a3851b32d0fd667a
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-07-21 23:57:50 +0200

    theme updated

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,12 @@ export const theme = {
     colors: {
         white: "#FFFFFF",
         alabaster: "#F7F7F7",
+        silverChalice: "#A3A3A3",
+
+        blizzardBlue: "#B3D1EF",
+        cornflowerBlue: "#4F759B",
+        richBlack: "#071E22",
+
         doveGray: "#666666",
         mineShaft: "#222222",
         black: "#000000",
