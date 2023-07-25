98fbb038c3aca22cc265792a6bb06e019d6717af
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-07-25 22:48:43 +0200

    CategoryList styling changed

--- src/features/CategoryList/Category/styled.js
+++ src/features/CategoryList/Category/styled.js
@@ -5,8 +5,15 @@ const mobileBP = ({ theme }) => theme.breakpoints.mobileMax;
 export const Link = styled.a`
   text-decoration: none;
   cursor: pointer;
+  font-size: 22px;
 `;
 
 export const Title = styled.h3`
     color: ${({ theme }) => theme.colors.doveGray};
+    font-weight: normal;
+
+    @media (max-width: ${mobileBP}px) {
+      font-size: 22px;
+      margin: 15px 0 15px 0;
+  }
 `;
