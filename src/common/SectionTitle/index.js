5d6aa8de8a91319b6dff06e10e9147aca8d8abf3
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-07-26 22:45:47 +0200

    SectionTitle added

---
+++ src/common/SectionTitle/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const mobileBP = ({ theme }) => theme.breakpoints.mobileMax;
+
+export const SectionTitle = styled.h2`
+  display: grid;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.doveGray};
+  font-size: 35px;
+  font-weight: normal;
+  margin: 50px 0 10px 0;
+
+  @media (max-width: ${mobileBP}px) {
+    font-size: 26px;
+    margin: 30px 0 6px 0;
+  }
+`;
