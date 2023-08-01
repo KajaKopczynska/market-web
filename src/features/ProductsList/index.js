dc39089e8deebc00112aedcc58aa2fd98235f634
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-08-01 14:42:12 +0200

    added new section in ProductList

--- src/features/ProductsList/index.js
+++ src/features/ProductsList/index.js
@@ -5,15 +5,33 @@ import { ProductListWrapper } from './styled';
 import ProductTile from './ProductTile';
 // import { CartContext } from "@/App"
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, products2 }) => {
     // const { cart } = useContext(CartContext)
+
+    const bestProducts = products.filter((product) => product.extra === "Best Selling Books");
+    const popularProducts = products.filter((product) => product.extra === "What Everyone's Talking About");
 
     return (
         <>
             <Container>
                 <SectionTitle>Best Selling Books</SectionTitle>
                 <ProductListWrapper>
-                    {products.map(product => (
+                    {bestProducts.map(product => (
+                        <ProductTile
+                            key={product.id}
+                            imgSrc={product.image}
+                            title={product.title}
+                            author={product.author}
+                            price={product.price}
+                            link={`/products/${product.id}`}
+                        // onAddToCart={() => onAddToCart(product)}
+                        // isInCart={cart.products.some(p => p.id === product.id)}
+                        />
+                    ))}
+                </ProductListWrapper>
+                <SectionTitle>What Everyone's Talking About</SectionTitle>
+                <ProductListWrapper>
+                {popularProducts.map(product => (
                         <ProductTile
                             key={product.id}
                             imgSrc={product.image}
