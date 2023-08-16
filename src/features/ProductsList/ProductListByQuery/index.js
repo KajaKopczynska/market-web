b3eca4ecdbfc875f6422218c8cf0992bd34f296c
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-08-16 16:04:56 +0200

    add no results message to ProductListByQuery

--- src/features/ProductsList/ProductListByQuery/index.js
+++ src/features/ProductsList/ProductListByQuery/index.js
@@ -3,8 +3,6 @@ import { Container } from '../../../common/Container';
 import { SectionTitle } from '../../../common/SectionTitle';
 import ProductTile from '../ProductTile';
 import { ProductListWrapper } from '../styled';
-import { selectSearchQuery } from '../../Search/searchSlice';
-import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 const ProductListByQuery = ({ products }) => {
@@ -18,21 +16,25 @@ const ProductListByQuery = ({ products }) => {
 
     return (
         <>
-            <Container>
-                <SectionTitle>Search Results for "{query}"</SectionTitle>
-                <ProductListWrapper>
-                    {filteredProducts.map(product => (
-                        <ProductTile
-                            key={product.id}
-                            imgSrc={product.image}
-                            title={product.title}
-                            author={product.author}
-                            price={product.price}
-                            link={`/products/${product.id}`}
-                        />
-                    ))}
-                </ProductListWrapper>
-            </Container>
+            {filteredProducts.length === 0 ? (
+                <SectionTitle>Sorry, there are no results for "{query}" 😞</SectionTitle>
+            ) : (
+                <Container>
+                    <SectionTitle>Search Results for "{query}"</SectionTitle>
+                    <ProductListWrapper>
+                        {filteredProducts.map(product => (
+                            <ProductTile
+                                key={product.id}
+                                imgSrc={product.image}
+                                title={product.title}
+                                author={product.author}
+                                price={product.price}
+                                link={`/products/${product.id}`}
+                            />
+                        ))}
+                    </ProductListWrapper>
+                </Container>
+            )}
         </>
     );
 };
